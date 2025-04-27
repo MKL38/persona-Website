8ef9524623e1e5606a3f3947535ebaa5dce45e7e
Don Teerapat <futhepteerapat@gmail.com>
2025-04-28 00:49:23 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,124 @@
+import "./App.css";
+import Navbar from "../src/componentUI/navBar";
+
+function App() {
+  return (
+    <>
+      <div className="body-container md:flex md:flex-row  lg:flex lg:flex-row">
+        {/* Mobile Navbar */}
+        <Navbar />
+        {/* large Navbar */}
+        <div className="bgNav hidden md:bg-[#cb0000] h-screen md:flex justify-start flex-col lg:w-[220px] lg:flex lg:justify-start fixed z-50">
+          <div className="nav-content flex-col ml-6 mt-10 md:mt-24 text-start text-white ">
+            <h1 className="lg:text-2xl  font-bold mb-10 ">introduction</h1>
+            <h1 className="lg:text-2xl font-bold mb-10 ">Skills</h1>
+            <h1 className="lg:text-2xl  font-bold  ">experiences</h1>
+            <div className="experience-ls text-end mr-16">
+              <p className="text-xl ">Ascend</p>
+              <p className="text-xl  ">CP Axtra</p>
+            </div>
+          </div>
+        </div>
+        {/* Hero section */}
+        <div className="hero-container-mobile mt-[80px] flex flex-row md:ml-[220px] space-x-10">
+          {/* Main Nickname */}
+          <div className="main-Nickname text-9xl ml-4 drop-shadow-2xl lg:w-[200px]">
+            <h1>D</h1>
+            <h1>O</h1>
+            <h1>N</h1>
+          </div>
+          {/* Hero img */}
+          <div className="hero-img sm:w-[250px] xl:w-[400px] mt-7 drop-shadow-2xl">
+            <img
+              className="hero-img-img object-cover rounded-sm"
+              src="src\img\6A6E5A14-FC0C-4A67-B039-D756CE81E248.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        {/* introduction content */}
+        <div className="intro-container mt-10 md:w-[400px] lg:w-full md:mt-[100px] lg:space-y-6">
+          <div className="intro-text-container text-start ml-6 space-y-4">
+            <h1 className="text-[#cb0000] font-bold">Who am I ?</h1>
+            <h1 className="mb-10">Mr.Teerapat Futhep</h1>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+              amet molestias iste maxime doloremque dolorum suscipit minus quae
+              vel excepturi similique odio! Voluptate sunt temporibus iusto.
+              Unde sint doloribus iure.
+            </span>
+            <div className="intro-text-container-education mt-10 space-y-3">
+              <h1 className="text-[#cb0000] font-bold">Education</h1>
+              <p>Techup</p>
+              <p>King Mongkut's Institute of Technology Ladkrabang</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Skills section*/}
+      <div className="grid lg:grid-cols-2 xl:ml-[220px] xl:mt-20 lg:ml-[180px] lg:mt-20 md:mt-24 md:grid md:grid-cols-1 ">
+        {/* Business */}
+        <div className="businessSkills-container  xl:w-100%">
+          <div className="skills-header bg-[#cb0000] xl:ml-32 xl:mr-4 lg:ml-32 lg:mr-4 py-3">
+            <h1 className="text-end mr-4 text-3xl font-bold text-white ">
+              Business
+            </h1>
+          </div>
+          {/* business skill content */}
+          <div className="skill-box text-end mt-10 md:mr-10 lg:mr-4 ml-4 mr-2">
+            <ul className="business-skills space-y-4">
+              <li>Competitive market analysis</li>
+              <li>Strong analytical thinking</li>
+              <li>Project management (retail and digital retail)</li>
+              <li>Business development for retail and wholesale industries</li>
+              <li>Strategy planing and development</li>
+              <li>Team leadership and assessment</li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Technical */}
+        <div className="businessSkills-container  xl:w-100% md:mt-10 lg:mt-0 ">
+          <div className="skills-header bg-[#cb0000] xl:mr-32 xl:ml-4 lg:mr-32 lg:ml-4 py-3   ">
+            <h1 className="xl:text-start lg:text-start text-end ml-4 text-3xl font-bold text-white md:text-end mr-4 ">
+              Technical
+            </h1>
+          </div>
+          {/* technical skill content */}
+          <div className="skill-box text-end mt-10 md:mr-10 mr-4 ml-4 md:text-end xl:text-start lg:text-start">
+            <ul className="front-end-skills">
+              <li className="font-bold">Frontend</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>Tailwind CSS</li>
+              <li>React</li>
+            </ul>
+
+            <ul className="back-end-skills mt-10">
+              <li className="font-bold">Backend</li>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>MongoDB</li>
+              <li>PostgresSQL</li>
+              <li>Next Auth</li>
+            </ul>
+
+            <ul className="other-skills mt-10">
+              <li className="font-bold">Other</li>
+              <li>Git / Github</li>
+            
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* Experience section */}
+      <div className="experience-container xl:ml-[220px] xl:mt-20 lg:ml-[180px] lg:mt-20 md:mt-24">
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentUI/navBar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Menu, X } from "lucide-react";
+
+const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(true);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  return (
+    <>
+      <div className="md:hidden lg:hidden">
+        {/* Navigation Toggle Button (visible on mobile) */}
+        <button
+          className="md:hidden fixed top-4 left-4 z-50 bg-red-700 text-white p-2 rounded-md"
+          onClick={toggleNav}
+        >
+          {isNavOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+
+        {/* Red Navigation Bar */}
+        <div
+          className={`${isNavOpen ? "translate-x-0" : "-translate-x-full "} 
+        } transition-transform duration-300 ease-in-out bg-red-700 text-white w-full md:w-64 fixed md:static h-screen z-40`}
+        >
+          <nav className="flex flex-col h-full pt-20 w-full ">
+            <div className="text-3xl font-bold mb-10">introduction</div>
+
+            <div className="text-3xl font-bold mb-10">Skills</div>
+
+            <div className="experience-container-mobile">
+              <div className="text-3xl font-bold pb-2">experiences</div>
+              <div className="nav-content-mobile pl-24 space-y-2 ">
+                <div className="text-xl ">Ascend</div>
+                <div className="text-xl  ">CP Axtra</div>
+              </div>
+            </div>
+
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
